62d4fc3900e94aedd4b1088fe0db4407853a86b2
ywl <young.lee@ucb.edu.bo>
2023-06-01 08:59:16 -0400

    EJERCICIO 1

--- src/presenter.js
+++ src/presenter.js
@@ -1,5 +1,4 @@
 import sumar from "./sumador";
-
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
